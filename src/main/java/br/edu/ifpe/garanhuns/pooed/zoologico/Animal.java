703a9b64e072e4dd27ec7e8851eeae2bf2d7408c
Fabricio Cabral <fabriciofx@gmail.com>
2015-09-24 17:39:54 -0300

    Adicionado o exemplo do Zoológico

---
+++ src/main/java/br/edu/ifpe/garanhuns/pooed/zoologico/Animal.java
@@ -0,0 +1,25 @@
+package br.edu.ifpe.garanhuns.pooed.zoologico;
+
+public class Animal {
+	String nome;
+	String raca;
+	String cor;
+	
+	public Animal(String n, String r, String c) {
+		nome = n;
+		raca = r;
+		cor = c;
+	}
+	
+	public void anda() {
+	}
+	
+	public void emiteSom() {
+	}
+	
+	public void corre() {
+	}
+	
+	public void come() {
+	}
+}
---
+++ src/main/java/br/edu/ifpe/garanhuns/pooed/zoologico/Leao.java
@@ -0,0 +1,25 @@
+package br.edu.ifpe.garanhuns.pooed.zoologico;
+
+import javax.swing.JOptionPane;
+
+public class Leao extends Animal {
+	public Leao(String n, String r, String c) {
+		super(n, r, c);
+	}
+	
+	public void anda() {
+		JOptionPane.showMessageDialog(null, "Anda de mansinho");
+	}
+	
+	public void emiteSom() {
+		JOptionPane.showMessageDialog(null, "GRRAAUUUUUU");
+	}
+	
+	public void corre() {
+		JOptionPane.showMessageDialog(null, "Corre rápidoooo");
+	}
+	
+	public void come() {
+		JOptionPane.showMessageDialog(null, "Come outros animais");
+	}
+}
