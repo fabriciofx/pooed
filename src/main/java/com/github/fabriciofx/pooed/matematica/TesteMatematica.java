218aa73d9ef950d7b4d823193e06e6f05e02e32b
Fabricio Cabral <fabriciofx@gmail.com>
2016-04-02 19:45:56 -0300

    Melhorias no teste do problema de matemática

--- src/main/java/com/github/fabriciofx/pooed/matematica/TesteMatematica.java
+++ src/main/java/com/github/fabriciofx/pooed/matematica/TesteMatematica.java
@@ -1,11 +1,31 @@
 package com.github.fabriciofx.pooed.matematica;
 
-import javax.swing.JOptionPane;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class TesteMatematica {
-	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, new Soma(2, 3, 4));
-		JOptionPane.showMessageDialog(null,
-				new Soma(new Multiplicacao(2, 3).resultado(), 4));
+	@Test
+	public void somaNumerosPositivos() {
+		Operacao soma = new Soma(2, 3);
+		assertEquals(5, soma.resultado(), 0.001);
+	}
+
+	@Test
+	public void somaNumerosNegativos() {
+		Operacao soma = new Soma(-2, -3);
+		assertEquals(-5, soma.resultado(), 0.001);
+	}
+
+	@Test
+	public void somaNumerosPositivosENegativos() {
+		Operacao soma = new Soma(2, -3);
+		assertEquals(-1, soma.resultado(), 0.001);
+	}
+
+	@Test
+	public void multiplicacaoESomaDeNumerosPositivos() {
+		Operacao multESoma = new Soma(new Multiplicacao(2, 3).resultado(), 4);
+		assertEquals(10, multESoma.resultado(), 0.001);
 	}
 }
